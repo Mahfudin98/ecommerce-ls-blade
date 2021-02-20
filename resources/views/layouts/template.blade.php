632dce911ef1f535656a120e3d55fac285e24409
Mahfudin98 <mahfudinnawawi@gmail.com>
2021-02-20 10:51:28 +0700

    update template fix icon

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>PT LAROSSA | @yield('title')</title>
-  <link rel="icon" href="img/icon.png" type="image/png">
+  <title>LS SKINCARE | @yield('title')</title>
+  <link rel="icon" href="{{asset('img/logo-ls.png')}}" type="image/png">
   <link rel="stylesheet" href="{{asset('template/vendors/bootstrap/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('template/vendors/nice-select/nice-select.css')}}">
   <link rel="stylesheet" href="{{asset('template/vendors/owl-carousel/owl.theme.default.min.css')}}">
