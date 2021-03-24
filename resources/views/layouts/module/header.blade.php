47cef86b6ed49d1b32a34a21ad0d8810a431abd3
Mahfudin98 <mahfudinnawawi@gmail.com>
2021-03-24 10:28:15 +0700

    update logo

--- resources/views/layouts/module/header.blade.php
+++ resources/views/layouts/module/header.blade.php
@@ -2,7 +2,7 @@
     <div class="main_menu">
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container">
-            <a class="navbar-brand logo_h" href="/"><img src="{{asset('img/logo-new.png')}}" alt=""></a>
+            <a class="navbar-brand logo_h" href="/"><img src="{{asset('img/logo-new.png')}}" width="120px" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="icon-bar"></span>
